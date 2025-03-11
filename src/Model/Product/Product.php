094ed78e0bc41361561d67b927a66bb543a2ad77
mostefa <termus96@gmail.com>
2025-03-11 22:02:06 +0100

    Rename Product model

--- src/Model/Product/Product.php
+++ src/Model/Product/Product.php
@@ -4,13 +4,8 @@ namespace App\Model\Product;
 
 use App\Model\Abstract\ProductModel;
 
-class SimpleProduct extends ProductModel
-{
-    public function getType(): string
-    {
-        return 'simple';
-    }
-    
+class Product extends ProductModel
+{    
     public function fromArray(array $data): self
     {
         $this->id = $data['id'];
@@ -22,7 +17,8 @@ class SimpleProduct extends ProductModel
         $this->attributes = $data['attributes'] ?? [];
         $this->prices = $data['prices'] ?? [];
         $this->brand = $data['brand'] ?? '';
-        
+        $this->typeName = $data['type_name'] ?? null;
+
         return $this;
     }
 }
